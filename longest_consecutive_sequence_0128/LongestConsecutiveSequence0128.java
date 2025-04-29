b33426f58854d327ed2c97831971a823bcec65a1
Kang MinJoo <sht000826@gmail.com>
2025-04-28 21:37:48 -0700

    longest solution using set

---
+++ longest_consecutive_sequence_0128/LongestConsecutiveSequence0128.java
@@ -0,0 +1,31 @@
+package longest_consecutive_sequence_0128;
+
+import java.util.*;
+
+public class LongestConsecutiveSequence0128 {
+    public static void main(String[] args) {
+        System.out.println(cal(new int[]{0,3,7,2,5,8,4,6,0,1}));
+    }
+
+    public static int cal(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+        System.out.println(set);
+        int maxLen = 0;
+        for(int num : set){
+            if(!set.contains(num-1)){
+                int currentLen = 1;
+                int current = num;
+
+                while(set.contains(current+1)){
+                    current++;
+                    currentLen ++;
+                }
+                maxLen = Math.max(currentLen, maxLen);
+            }
+        }
+        return maxLen;
+    }
+}
