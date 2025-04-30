f316a026f07e6984870f11705c7d3a4bf844fd55
Kang MinJoo <sht000826@gmail.com>
2025-04-30 08:55:27 -0700

    problem 3 Start_ java ver.

---
+++ longest_substring_without_repeating_characters_0003/LongestSubstringWithoutRepeatingCharacters0003.java
@@ -0,0 +1,16 @@
+package longest_substring_without_repeating_characters_0003;
+
+public class LongestSubstringWithoutRepeatingCharacters0003 {
+    public static void main(String[] args) {
+        System.out.println(solution("abcabcbb"));
+    }
+    public static int solution(String s){
+        int ans = 0;
+        char[] charr = s.toCharArray();
+        System.out.println(charr);
+//        Set<String> set = new LinkedHashSet<>(Arrays.asList(charr));
+
+
+        return ans;
+    }
+}
